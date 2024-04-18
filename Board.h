114ea1de28e7d45ef08e7863e78c96afaba2b8d0
Ishaan Joshi <ishaan.joshi@wsu.edu>
2024-04-18 16:07:54 -0700

    I added the function for the gameboard pieces of the characteer when they land on either a snake or a ladder

--- Board.h
+++ Board.h
@@ -2,11 +2,61 @@
 
 #include "GameObject.h"
 
-class Board : public GameObject, public sf::RectangleShape
+class cell : public GameObject, public sf::RectangleShape
 {
 public:
-	
-private:
+
+	bool ladder;
+	int x_destination;
+	int y_destination;
+	bool snake;
+
+
 
 };
 
+class Board
+{
+public:
+	std::vector <std::vector<cell>> board;
+
+	Board()
+	{
+		board = std::vector <std::vector<cell>>(10);
+		for (int i = 0; i < 10; i++)
+		{
+			board.at(i) = std::vector<cell>(10);
+		}
+	}
+
+	void player_moves(int x, int y, player& p)
+	{
+		board.at(x).at(y);
+
+		if (board.at(x).at(y).ladder)
+		{
+			p.x_cordinate = board.at(x).at(y).x_destination;
+			p.y_cordinate = board.at(x).at(y).y_destination;
+		}
+
+		if (board.at(x).at(y).snake)
+		{
+			p.x_cordinate = board.at(x).at(y).x_destination;
+			p.y_cordinate = board.at(x).at(y).y_destination;
+		}
+
+	}
+
+};
+
+class player
+{
+public:
+
+	std::string player;
+	int x_cordinate;
+	int y_cordinate;
+
+
+	
+};
--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main(void)
 			}
 		}
 	}
+	
 
 	return 0;
 }
