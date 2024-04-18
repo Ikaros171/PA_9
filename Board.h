3949b80fe95b0d72eda8763d71754799dd0816bc
Nathanael Zink <nathanael.zink@wsu.edu>
2024-04-18 16:32:53 -0700

    Ishaan's code.

--- Board.h
+++ Board.h
@@ -2,9 +2,14 @@
 
 #include "GameObject.h"
 
-#include <vector>
-
-using std::vector;
+class cell : public GameObject, public sf::RectangleShape
+{
+public:
+	bool ladder;
+	bool snake;
+	int x_destination;
+	int y_destination;
+};
 
 class Board : public GameObject
 {
@@ -12,9 +17,24 @@ public:
 	// constructor
 	Board(float x, float y, const string& fileName) : GameObject(x, y, fileName)
 	{
-
+		board = std::vector <std::vector<cell>>(10);
+		for (int i = 0; i < 10; i++)
+		{
+			board.at(i) = std::vector<cell>(10);
+		}
 	}
+
+	void player_moves(int x, int y, player& p);
+
 private:
-	
+	std::vector <std::vector<cell>> board;
 };
 
+class player
+{
+public:
+
+	std::string player;
+	int x_cordinate;
+	int y_cordinate;
+};
